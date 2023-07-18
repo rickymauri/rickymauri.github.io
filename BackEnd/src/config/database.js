2d2afefca1c2d78f0f4e7fe368450ce2deb2ee4a
rickymauri <129845197+rickymauri@users.noreply.github.com>
2023-07-18 10:29:05 +0200

    Delete BackEnd directory

--- BackEnd/src/config/database.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('progettoevoli', 'root', 'vivaifinley', {
-  dialect: 'mysql',
-  host: 'localhost',
-  logging: false,
-});
-
-module.exports = sequelize;
